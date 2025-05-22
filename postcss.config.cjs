a2f23f73454faa7a267f6726b9d4e0b931f8eafa
blakeox <blakeopowell@outlook.com>
2025-05-21 21:42:12 -0400

    refactor: Simplify PostCSS configuration and enhance ThemeToggle component styles and is now able to load locally

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,11 +1,6 @@
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
   plugins: {
-    tailwindcss: {
-      darkMode: 'class',
-      content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}', './public/**/*.html'],
-    },
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
