e69af702cc9192336eb92fae7030dcb227266cde
blakeox <blakeopowell@outlook.com>
2025-06-08 13:40:31 -0400

    refactor: Update content module imports and clean up About Me section layout for improved readability

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,12 +1,12 @@
 
 export default new Map([
-["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/LLM-note-coaching.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FLLM-note-coaching.mdx&astroContentModuleFlag=true")],
-["src/content/projects/Microsoft-Fabric.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FMicrosoft-Fabric.mdx&astroContentModuleFlag=true")],
-["src/content/projects/adp-workforcenow.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fadp-workforcenow.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/advancedmd-implementation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fadvancedmd-implementation.mdx&astroContentModuleFlag=true")],
+["src/content/projects/adp-workforcenow.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fadp-workforcenow.mdx&astroContentModuleFlag=true")],
+["src/content/projects/Microsoft-Fabric.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2FMicrosoft-Fabric.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/bank-projections-modeling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fbank-projections-modeling.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/enterprise-digital-transformation.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fenterprise-digital-transformation.mdx&astroContentModuleFlag=true")],
+["src/content/blog/hello-world.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fhello-world.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/ferment-app.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fferment-app.mdx&astroContentModuleFlag=true")],
 ["src/content/projects/sage-intacct-integration-with-square-pos.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fsage-intacct-integration-with-square-pos.mdx&astroContentModuleFlag=true")]]);
 		
